8b4826c7b7a8533f2f67bdce8ba3ff761447d3a6
stefan4472 <stefan.kussmaul@gmail.com>
2017-12-27 22:00:46 -0500

    Much Improved Map Rendering, But Still Buggy

--- map.cpp
+++ map.cpp
@@ -25,35 +25,49 @@ void Map::centerTo(SDL_Rect newCenter)
 
 void Map::drawTo(SDL_Surface* screenSurface) 
 {
+	printf("Centered on %d %d %d %d\n", center.x, center.y, center.w, center.h);
 	// virtual coordinates for top-left of view 
 	int top_left_x = center.x - (SCREEN_WIDTH - center.w) / 2;
 	int top_left_y = center.y - (SCREEN_HEIGHT - center.h) / 2;
+	printf("%d, %d\n", top_left_x, top_left_y);
 	
 	// offsets from tile borders on x and y
 	int offset_x = top_left_x % TILE_WIDTH;
 	int offset_y = top_left_y % TILE_HEIGHT;
+	printf("%d, %d\n", offset_x, offset_y);
 	
 	// calculate # of tiles to render on width and height of screen (todo: make const)
 	int tiles_wide = (SCREEN_WIDTH / TILE_WIDTH) + 1;
 	int tiles_tall = (SCREEN_HEIGHT / TILE_HEIGHT) + 1;
+	printf("%d, %d\n", tiles_wide, tiles_tall);
 	
-	// coordinates of top-left tile to render
+	// row and col of top-left tile to render
 	int start_tile_x = top_left_x / TILE_WIDTH;
 	int start_tile_y = top_left_y / TILE_HEIGHT;
+	printf("%d, %d\n", start_tile_x, start_tile_y);
 	
 	//printf("Finished Calculations\n");
 	
 	// render proper tiles, at offsets
 	for (int i = 0; i < tiles_tall; i++) 
 	{
-		dest.y = (i + start_tile_y) * TILE_HEIGHT - offset_y;
+		dest.y = i * TILE_HEIGHT - offset_y;
 		//printf("dest.y is %d\n", dest.y);
 		for (int j = 0; j < tiles_wide; j++) 
 		{
-			dest.x = (j + start_tile_x) * TILE_WIDTH - offset_x;
+			dest.x = j * TILE_WIDTH - offset_x;
 			//printf("dest.x is %d\n", dest.x);
-			SDL_BlitSurface( tileImgs[ mapTiles[i][j] ], &src, screenSurface, &dest );
-			//printf("Drew\n");
+			
+			// out of range: draw black
+			if (start_tile_y + i < 0 || start_tile_x + j < 0 || start_tile_y + i >= mapRows || start_tile_x + j >= mapCols) 
+			{
+				SDL_FillRect( screenSurface, &dest, SDL_MapRGB( screenSurface->format, 0x00, 0x00, 0x00 ) );
+			}
+			else 
+			{
+				SDL_BlitSurface( tileImgs[ mapTiles[start_tile_y + i][start_tile_x + j] ], &src, screenSurface, &dest );
+			}
+				//printf("Drew\n");
 		}
 	}
 	
